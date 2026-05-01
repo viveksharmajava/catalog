e651918842cb5539e7c0d652db20116a45d45836
viveksharmajava <viveksharma.svim@gmail.com>
2026-05-01 13:50:43 +0530

    set up project

---
+++ src/main/java/com/playpro/playpro/PlayproApplication.java
@@ -0,0 +1,13 @@
+package com.playpro.playpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playpro/playpro/PlayproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playpro.playpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
